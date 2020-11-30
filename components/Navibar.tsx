c660c110667bf1b4f01fad4449c8a0562eeb3cf1
Harry Headlam <nickname.co@gmail.com>
2020-11-29 22:00:57 -0600

    Navigation bar

---
+++ components/Navibar.tsx
@@ -0,0 +1,17 @@
+import { relative } from "path";
+
+const Navibar = (props: any) => (
+    <header style={{ backgroundColor: "#033f88", paddingTop: "0px", paddingRight: "0px" }}>
+        <nav style={{ backgroundColor: "#033f88", overflow: "hidden", border: 0, margin: "-1px", padding: 0 }}>
+            <div></div>
+            <div>
+                <img src="https://dallascityhall.com/PublishingImages/style-library/City%20of%20Dallas%20-%20Landscape%20-%20White%20and%20Green.png">
+                </img>
+
+            </div>
+
+        </nav>
+    </header>
+)
+
+export default Navibar;
