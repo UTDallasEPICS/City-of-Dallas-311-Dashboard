6f36a774348d560b22aa3a2031792b83cd813ce6
Harry Headlam <nickname.co@gmail.com>
2020-11-30 15:42:58 -0600

    nav bar added to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
 import FilterMenu from "../components/FilterMenu";
 import { Map } from '../components/Map';
 import React, { Component } from 'react';
+import Navibar from '../components/Navibar';
 
 class index extends Component {
-  render() {
-    return (
-      <div>
-        <div className="311-dashboard-container" style={{
-          display: 'grid', gridTemplateAreas: '"FilterMenu Map Map Map"'
-        }}>
-          < FilterMenu />
-          <Map />
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Navibar />
+                <div className="311-dashboard-container" style={{
+                    display: 'grid', gridTemplateAreas: '"FilterMenu Map Map Map"'
+                }}>
+                    < FilterMenu />
+                    <Map />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default index;
