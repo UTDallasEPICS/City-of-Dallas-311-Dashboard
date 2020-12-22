317bd530ac69182a38f5f29bfdd6efb660c40770
Harry Headlam <nickname.co@gmail.com>
2020-12-22 10:43:54 -0600

    added both buttons and comments; fixed spelling

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useImperativeHandle, useState } from 'react';
 import { SeacrhLocationBox, SearchRequestNumberBox } from './SearchBox';
 import Button from 'react-bootstrap/Button'
 import { propTypes } from 'react-bootstrap/esm/Image';
@@ -9,8 +9,17 @@ function Filter({ title, items }: { title: any, items: any }) {
     const [selection, setSelection] = useState([]);
     const toggle = (e: any) => setOpen(!open);
 
-    function handleClick(item: any) {
-
+    function handleClick(item: never) {
+        if (!selection.some(current => current === item)) {
+            setSelection([item]);
+        } else {
+            let selectionAfterRemoval = selection;
+            selectionAfterRemoval = selectionAfterRemoval.filter(
+                current => current !== item
+            );
+            setSelection([...selectionAfterRemoval]);
+        }
+        console.log(selection);
     }
 
     return (
@@ -20,9 +29,14 @@ function Filter({ title, items }: { title: any, items: any }) {
             </Button>
             <div>{!open ?
                 '' :
-                <div>
-                    {items}
-                </div>
+                <ul style={{ listStyleType: 'none', alignContent: 'left' }}>
+                    {items.map((item: any) => (
+                        <li className="item-button" key={item.title} style={{ textAlign: 'left' }}>
+                            <input type="radio" name={title} defaultChecked={selection === item.title ? true : false} onClick={() => handleClick(item)} />
+                            <label>{item.title}</label>
+                        </li>
+                    ))}
+                </ul>
             }
             </div>
         </div>
--- components/FilterHub.tsx
+++ components/FilterHub.tsx
@@ -1,53 +1,97 @@
 import Filter from "../components/Filter";
-import { SeacrhLocationBox, SearchRequestNumberBox } from './SearchBox';
+import { SearchLocationBox, SearchRequestNumberBox } from './SearchBox';
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+/* Format for items
+{
+    title: string               name of button
+    content: [string]           keywords to search for(in Departments include acronym and other shorthand used in salesforce)
+                                                      (in Requests further investigation is needed )
+}
+*/
 const Departments = [
-    "Fire", "water"
+    {
+        title: 'All',
+        content: ['All']
+    },
+    {
+        title: 'Fire Department',
+        content: ['Fire Department', 'DFR'],
+    },
+    {
+        title: 'Code Compliance',
+        content: ['Code Compliance', 'CCS']
+    }
 ];
+const Requests = [
+    {
+        title: 'All',
+        content: ['All'],
 
-const Types = [
-    "All",
+    },
+    {
+        title: 'Graffiti',
+        content: ['Graffiti']
+    },
+
 ];
+/*NOTE 
+  there might be a better way to do this. You could use a loop to go through the data given from the API to generate the list of
+  search types/departments dynamically, rather than by hand. Todo do this requires using the fetch API and connecting it to API made spring20.
+  Then add them into a list and check for duplicates, afterwards feed them into the items depicted above. Previously designed code 
+  should do the rest if in correct format.
+*/
 
-var searchLocation;
-var searchRequestNumber;
+var searchLocation: string;
+var searchRequestNumber: string;
 
 
 class FilterHub extends Component {
 
     getDepartmentValue = () => {
-
+        //TODO
+        //return selection variable in filter titled departments
     };
 
     getTypeValue = () => {
-
+        //TODO
+        //return selection variable in filter titled Request Types
     };
 
     getDistrictValue = () => {
-
+        //TODO
+        //get district value from form for districts (implemnented as 'html select' or 'slider' (if slider not default))
     };
 
     handleLocationInput = (e: any) => {
         console.log(e.target.value);
         searchLocation = e.target.value
-    };
+    };//Set location
 
     handleRequestNumberInput = (e: any) => {
         console.log(e.target.value);
         searchRequestNumber = e.target.value;
-    };
+    };//Set Request Number
 
+    search = () => {
+        //TODO 
+        console.log(searchLocation);
+        console.log(searchRequestNumber);
+    }
 
     render() {
         return (
             <div style={{ width: 250, border: 'solid', textAlign: 'center' }}>
                 <h1>Search</h1>
                 <br></br>
-                <SeacrhLocationBox handleInput={this.handleLocationInput} />
+                <br></br>
+                <SearchLocationBox handleInput={this.handleLocationInput} />
                 <SearchRequestNumberBox handleInput={this.handleRequestNumberInput} />
                 <Filter title="Departments" items={Departments} />
+                <Filter title="Request Types" items={Requests} />
+                <button onClick={this.search}>Search</button>
             </div>
         )
     }
